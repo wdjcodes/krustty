15fc5497f423885115fd20c6e3f06e72eb02beed
Derek Johnson <wdjcodes@gmail.com>
2026-03-08 22:25:56 -0400

    feat: krustty spawns tty and sends input and reads output
    added rtrb for efficient copying of slicees between threads added
    portable-pty to handle opening and managing the pty device

--- krustty/src/main.rs
+++ krustty/src/main.rs
@@ -1,3 +1,33 @@
+use rtrb::CopyToUninit;
+
+use crate::terminal::Terminal;
+
+const MAX_LINE_LENGTH: usize = 4096;
+
+mod terminal;
 fn main() {
-    println!("Hello, world!");
+    let mut term = Terminal::spawn("zsh");
+
+    let mut buffer = String::with_capacity(MAX_LINE_LENGTH);
+    let stdin = std::io::stdin();
+
+    println!("You can now type commands for Bash (type 'exit' to quit):");
+
+    // Main thread sends user input to the writer thread.
+    loop {
+        buffer.clear();
+        let num = stdin.read_line(&mut buffer).unwrap();
+        if buffer.trim() == "exit" {
+            break;
+        }
+        if let Ok(mut chunk) = term.input.write_chunk_uninit(num) {
+            let (slice1, slice2) = chunk.as_mut_slices();
+            let wrap = slice1.len();
+            buffer.as_bytes()[..wrap].copy_to_uninit(slice1);
+            buffer.as_bytes()[wrap..].copy_to_uninit(slice2);
+            unsafe { chunk.commit(num) };
+        };
+    }
+
+    term.close();
 }
