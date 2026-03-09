e920b381db871a1fd34f9b46affd2c4aa5351b44
Derek Johnson <wdjcodes@gmail.com>
2026-03-08 22:24:39 -0400

    fixed linting and formatting

--- krustty/src/main.rs
+++ krustty/src/main.rs
@@ -1,10 +1,8 @@
-
 use rtrb::CopyToUninit;
 
 use crate::terminal::Terminal;
 
 const MAX_LINE_LENGTH: usize = 4096;
-
 
 mod terminal;
 fn main() {
@@ -12,7 +10,6 @@ fn main() {
 
     let mut buffer = String::with_capacity(MAX_LINE_LENGTH);
     let stdin = std::io::stdin();
-
 
     println!("You can now type commands for Bash (type 'exit' to quit):");
 
@@ -23,12 +20,12 @@ fn main() {
         if buffer.trim() == "exit" {
             break;
         }
-        if let Ok(mut chunk) = term.input.write_chunk_uninit(num){
+        if let Ok(mut chunk) = term.input.write_chunk_uninit(num) {
             let (slice1, slice2) = chunk.as_mut_slices();
             let wrap = slice1.len();
             buffer.as_bytes()[..wrap].copy_to_uninit(slice1);
             buffer.as_bytes()[wrap..].copy_to_uninit(slice2);
-            unsafe {chunk.commit(num)};
+            unsafe { chunk.commit(num) };
         };
     }
 
--- krustty/src/terminal.rs
+++ krustty/src/terminal.rs
@@ -1,24 +1,28 @@
-use std::{io::{Read, Write}, thread::{self, JoinHandle}};
+use std::{
+    io::{Read, Write},
+    thread::{self, JoinHandle},
+};
 
 use portable_pty::{Child, CommandBuilder, NativePtySystem, PtySize, PtySystem};
 use rtrb::{Consumer, Producer, RingBuffer};
-
 
 pub struct Terminal {
     _pty_reader: JoinHandle<()>,
     _pty_writer: JoinHandle<()>,
     child: Box<dyn Child + Send + Sync>,
-    pub input: Producer<u8>
+    pub input: Producer<u8>,
 }
 
 impl Terminal {
     pub fn spawn(cmd: &str) -> Self {
-        let pty = NativePtySystem::default().openpty(PtySize {
-            rows: 24,
-            cols: 80,
-            pixel_width: 0,
-            pixel_height: 0,
-        }).unwrap();
+        let pty = NativePtySystem::default()
+            .openpty(PtySize {
+                rows: 24,
+                cols: 80,
+                pixel_width: 0,
+                pixel_height: 0,
+            })
+            .unwrap();
         let cmd = CommandBuilder::new(cmd);
         let child = pty.slave.spawn_command(cmd).unwrap();
         drop(pty.slave);
@@ -27,10 +31,10 @@ impl Terminal {
         let std_out = pty.master.try_clone_reader().unwrap();
         let (writer, reader) = RingBuffer::<u8>::new(4096);
         Self {
-            _pty_reader: thread::spawn(move|| read_pty(std_out)),
-            _pty_writer: thread::spawn(move|| write_pty(std_in, reader)),
+            _pty_reader: thread::spawn(move || read_pty(std_out)),
+            _pty_writer: thread::spawn(move || write_pty(std_in, reader)),
             child,
-            input: writer
+            input: writer,
         }
     }
 
@@ -46,9 +50,9 @@ pub fn write_pty(mut std_in: Box<dyn Write + Send>, mut reader: Consumer<u8>) {
     loop {
         let chunk = reader.read_chunk(reader.slots()).unwrap();
         let (slice1, slice2) = chunk.as_slices();
-        if slice1.len() > 0 { 
+        if !slice1.is_empty() {
             std_in.write_all(slice1).unwrap();
-            if slice2.len() > 0 { 
+            if !slice2.is_empty() {
                 std_in.write_all(slice2).unwrap();
             }
         }
@@ -60,7 +64,7 @@ pub fn read_pty(mut std_out: Box<dyn Read + Send>) {
     let mut buffer = [0u8; 1024];
     loop {
         match std_out.read(&mut buffer) {
-            Ok(0) =>  break, // EOF
+            Ok(0) => break, // EOF
             Ok(n) => {
                 let output = String::from_utf8_lossy(&buffer[..n]);
                 print!("{}", output); // Print to stdout for visibility.
