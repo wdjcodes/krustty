19e4ee4fb6a686628a1668ff97768423bad7d381
Derek Johnson <wdjcodes@gmail.com>
2026-03-11 23:05:11 -0400

    feat: add ui window to krustty (#12)
    Open ui window with winit and readner blank screen with wgpu. Text is
    still rendered in hosting terminal, but typing exit sends a
    CloseRequested event to the ui event loop to close the program.

--- krustty/src/main.rs
+++ krustty/src/main.rs
@@ -1,33 +1,45 @@
-use rtrb::CopyToUninit;
+use std::error::Error;
 
-use crate::terminal::Terminal;
+use rtrb::CopyToUninit;
+use winit::event_loop::EventLoop;
+
+mod ui;
+
+use crate::{terminal::Terminal, ui::Application};
 
 const MAX_LINE_LENGTH: usize = 4096;
 
 mod terminal;
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
+    env_logger::init();
     let mut term = Terminal::spawn("zsh");
 
     let mut buffer = String::with_capacity(MAX_LINE_LENGTH);
     let stdin = std::io::stdin();
 
     println!("You can now type commands for Bash (type 'exit' to quit):");
-
+    let event_loop = EventLoop::with_user_event().build()?;
+    let proxy = event_loop.create_proxy();
     // Main thread sends user input to the writer thread.
-    loop {
-        buffer.clear();
-        let num = stdin.read_line(&mut buffer).unwrap();
-        if buffer.trim() == "exit" {
-            break;
+    std::thread::spawn(move || {
+        loop {
+            buffer.clear();
+            let num = stdin.read_line(&mut buffer).unwrap();
+            if buffer.trim() == "exit" {
+                break;
+            }
+            if let Ok(mut chunk) = term.input.write_chunk_uninit(num) {
+                let (slice1, slice2) = chunk.as_mut_slices();
+                let wrap = slice1.len();
+                buffer.as_bytes()[..wrap].copy_to_uninit(slice1);
+                buffer.as_bytes()[wrap..].copy_to_uninit(slice2);
+                unsafe { chunk.commit(num) };
+            };
         }
-        if let Ok(mut chunk) = term.input.write_chunk_uninit(num) {
-            let (slice1, slice2) = chunk.as_mut_slices();
-            let wrap = slice1.len();
-            buffer.as_bytes()[..wrap].copy_to_uninit(slice1);
-            buffer.as_bytes()[wrap..].copy_to_uninit(slice2);
-            unsafe { chunk.commit(num) };
-        };
-    }
-
-    term.close();
+        term.close();
+        let _ = proxy.send_event(ui::Event::CloseRequested);
+    });
+    let mut app = Application::new(&event_loop);
+    event_loop.run_app(&mut app)?;
+    Ok(())
 }
