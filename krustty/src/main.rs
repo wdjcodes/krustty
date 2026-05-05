1f90ada8b88b7b58d312a8ce0f3b3f6364c5f0db
Derek Johnson <wdjcodes@gmail.com>
2026-05-04 21:45:38 -0400

    fix: replace fontconfig provider to use native library
    Switch from rust-fontconfig to fontconfig. Have 2 pull requests waiting
    to be accepted in the fontconfig library. The first is for charset
    support. This is completely necessary as fontconfig does not support it
    by default. The second allows easier use of custom patterns, if not
    accepted can use more verbose code pattern.

--- krustty/src/main.rs
+++ krustty/src/main.rs
@@ -1,3 +1,4 @@
+use tracing_subscriber::EnvFilter;
 use winit::event_loop::EventLoop;
 
 mod color;
@@ -9,7 +10,13 @@ mod ui;
 use crate::ui::Application;
 
 fn main() -> anyhow::Result<()> {
-    tracing_subscriber::fmt().with_line_number(true).init();
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| EnvFilter::new("off,krustty=info")),
+        )
+        .with_line_number(true)
+        .init();
 
     let event_loop = EventLoop::with_user_event().build()?;
     let mut app = Application::new(&event_loop);
--- krustty/src/ui/font.rs
+++ krustty/src/ui/font.rs
@@ -1,7 +1,6 @@
 use std::{collections::HashMap, rc::Rc};
 
-use rust_fontconfig::FcPattern;
-use tracing::debug;
+use tracing::{debug, error, info, warn};
 
 use crate::ui::{GpuHandle, texture::Texture};
 
@@ -11,6 +10,106 @@ pub struct CachedGlyph {
     pub y: u32,
 }
 
+pub struct FontCache {
+    fonts: Vec<Font>,
+    fc: fontconfig::Fontconfig,
+}
+
+impl FontCache {
+    /// Initialize the FontCache loading the systems default monospace font
+    /// # Panics
+    /// If no font is found to initialize the cache with the function will panic
+    fn new() -> Self {
+        let fc = fontconfig::Fontconfig::new().expect("Failed to initalize fontconfig");
+        let font_desc = fc
+            .find("monospace", None)
+            .expect("Could not find a monospace font, krustty is not currently shipped with one");
+
+        info!(
+            "Selected font: {} index {:?} path {:?}",
+            font_desc.name, font_desc.index, font_desc.path
+        );
+
+        let font_bytes = std::fs::read(&font_desc.path).unwrap_or_else(|_| {
+            panic!(
+                "Failed to read font file: {}",
+                font_desc.path.to_string_lossy()
+            )
+        });
+
+        let f = fontdue::Font::from_bytes(font_bytes.as_ref(), Default::default()).unwrap();
+        let font = Font::new(f);
+        let fonts = vec![font];
+
+        Self { fonts, fc }
+    }
+
+    /// Returns the index for the font to be used to render the provided glyph. If one has not
+    /// already been loaded searches the system for a monospace font that provides
+    /// it. If none is found this returns the default font (`self.fonts[0]`) with
+    /// the expectation that the default symbol will be rendered from that font.
+    fn select_font(&mut self, c: char) -> &Font {
+        if let Some(pos) = self.fonts.iter().position(|f| f.font.has_glyph(c)) {
+            &self.fonts[pos]
+        } else {
+            if let Some(f) = self.load_font(c) {
+                self.fonts.push(f);
+                self.fonts.last().unwrap()
+            } else {
+                &self.fonts[0]
+            }
+        }
+    }
+
+    /// Tries to load a font that contains the specified glyph.
+    /// Returns `None` if one was not found on the system
+    fn load_font(&mut self, c: char) -> Option<Font> {
+        let mut pat = fontconfig::Pattern::new(&self.fc);
+        pat.add_string(fontconfig::FC_FAMILY, c"monospace");
+        let mut cs = fontconfig::CharSet::create();
+        cs.add_char(c);
+        pat.add_charset(cs);
+        let font_desc = if let Some(fd) = self.fc.find_pattern(&mut pat) {
+            fd
+        } else {
+            warn!("Failed to find a font for: u+{:04x}", c as u32);
+            return None;
+        };
+
+        info!(
+            "Selected font: {} index {:?} path {:?}",
+            font_desc.name, font_desc.index, font_desc.path
+        );
+
+        let font_bytes = if let Ok(font_bytes) = std::fs::read(&font_desc.path) {
+            font_bytes
+        } else {
+            error!(
+                "Failed to read font file: {}",
+                font_desc.path.to_string_lossy()
+            );
+            return None;
+        };
+
+        let f = fontdue::Font::from_bytes(font_bytes.as_ref(), Default::default()).unwrap();
+        Some(Font::new(f))
+    }
+}
+pub struct Font {
+    pub font: fontdue::Font,
+    pub baseline: f32,
+}
+
+impl Font {
+    fn new(font: fontdue::Font) -> Self {
+        let baseline = font
+            .horizontal_line_metrics(FONT_PX)
+            .expect("Failed to get font metrics")
+            .ascent;
+        Self { font, baseline }
+    }
+}
+
 /// provides mechanisms to resolve a single character to a rendered
 /// glyph to make text rendering fast and easy on the gpu
 pub struct GlyphCache {
@@ -18,53 +117,37 @@ pub struct GlyphCache {
     cache: HashMap<char, CachedGlyph>,
     atlas: AtlasData,
     texture: Option<Rc<Texture>>,
+    fonts: FontCache,
 }
 
-const FONT_PX: f32 = 16.0;
+const FONT_PX: f32 = 14.666667;
 
 impl GlyphCache {
     pub fn new(atlas_size: u32, cell_width: u32, cell_height: u32) -> Self {
-        let fc = rust_fontconfig::FcFontCache::build();
-        let font_match = fc
-            .query(
-                &FcPattern {
-                    monospace: rust_fontconfig::PatternMatch::True,
-                    ..Default::default()
-                },
-                &mut Vec::new(),
-            )
-            .expect("Could not find a monospace font, krustty is not currently shipped with one");
-
-        let font_bytes = fc
-            .get_font_bytes(&font_match.id)
-            .expect("Error loading font");
-
-        let font = fontdue::Font::from_bytes(font_bytes, Default::default()).unwrap();
-        let atlas = AtlasData::new(font, atlas_size, cell_width, cell_height);
+        let fonts = FontCache::new();
+        let atlas = AtlasData::new(atlas_size, cell_width, cell_height);
         Self {
             cache: HashMap::new(),
             atlas,
             texture: None,
+            fonts,
         }
     }
 
     // Loads all of the common printable english ascii characters into the cache
     pub fn load_ascii(&mut self) {
         for c in '!'..='~' {
-            let glyph = self.atlas.load_glyph(c, FONT_PX);
+            let font = self.fonts.select_font(c);
+            let glyph = self.atlas.load_glyph(c, font, FONT_PX);
             self.cache.insert(c, glyph);
         }
     }
 
     pub fn get(&mut self, c: char) -> &CachedGlyph {
-        let atlas = &mut self.atlas;
+        let font = self.fonts.select_font(c);
         self.cache
             .entry(c)
-            .or_insert_with(|| atlas.load_glyph(c, FONT_PX))
-    }
-
-    pub fn atlas_data(&self) -> &[u8] {
-        &self.atlas.data
+            .or_insert_with(|| self.atlas.load_glyph(c, font, FONT_PX))
     }
 
     pub fn atlas_size(&self) -> u32 {
@@ -92,7 +175,7 @@ impl GlyphCache {
         }
 
         let texture = self.get_atlas_or_init(&gpu.device);
-        texture.write_texture(&gpu.queue, self.atlas_data());
+        texture.write_texture(&gpu.queue, &self.atlas.data);
         self.clean();
     }
 }
@@ -105,16 +188,10 @@ struct AtlasData {
     pub cell_width: u32,
     pub cell_height: u32,
     pub dirty: bool,
-    pub font: fontdue::Font,
-    baseline: u32,
 }
 
 impl AtlasData {
-    fn new(font: fontdue::Font, size: u32, cell_width: u32, cell_height: u32) -> Self {
-        let baseline = font
-            .horizontal_line_metrics(FONT_PX)
-            .expect("Failed to get font metrics")
-            .ascent as u32;
+    fn new(size: u32, cell_width: u32, cell_height: u32) -> Self {
         Self {
             data: vec![0_u8; (size * size) as usize],
             next_index: 0,
@@ -122,25 +199,30 @@ impl AtlasData {
             cell_width,
             cell_height,
             dirty: false,
-            font,
-            baseline,
         }
     }
     /// Rasterizes a char and packs it into the atlas.
     /// Returns the pixel coordinates of the new glyph.
-    pub fn load_glyph(&mut self, c: char, px: f32) -> CachedGlyph {
-        let (metrics, bitmap) = self.font.rasterize(c, px);
+    pub fn load_glyph(&mut self, c: char, font: &Font, px: f32) -> CachedGlyph {
+        let (metrics, bitmap) = font.font.rasterize(c, px);
         // Calculate grid position
         let cols = self.atlas_size / self.cell_width;
         let row = self.next_index / cols;
         let col = self.next_index % cols;
 
         // Calculate the offset needed to place glyph on baseline
-        let y_off = self.baseline as i32 - (metrics.ymin + metrics.height as i32);
+        let y_off = font.baseline as i32 - (metrics.ymin + metrics.height as i32);
 
         debug!(
-            "Char: {} Baseline: {} y_off: {} height: {} y_min: {} x_min: {} width: {}",
-            c, self.baseline, y_off, metrics.height, metrics.ymin, metrics.xmin, metrics.width
+            "Char: {} U+{:04x} Baseline: {} y_off: {} height: {} y_min: {} x_min: {} width: {}",
+            c,
+            c as u32,
+            font.baseline,
+            y_off,
+            metrics.height,
+            metrics.ymin,
+            metrics.xmin,
+            metrics.width
         );
 
         let atlas_x = col * self.cell_width;
