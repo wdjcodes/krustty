88236b7895144bd7e8fb305e2573d880c09e3f97
Derek Johnson <wdjcodes@gmail.com>
2026-05-08 21:52:50 -0400

    refactor: change default theme to look like breeze
    add color-hex as a dependency and create a simple declarative macro to
    make defining palette Srgb values easier. Change default theme to
    breeze.

--- krustty/src/color.rs
+++ krustty/src/color.rs
@@ -2,61 +2,73 @@ use palette::Srgb;
 pub type Rgb = Srgb;
 
 pub struct ColorProfile {
-    pub fg: Srgb<u8>,
-    pub bg: Srgb<u8>,
-    pub black: Srgb<u8>,
-    pub red: Srgb<u8>,
-    pub green: Srgb<u8>,
-    pub yellow: Srgb<u8>,
-    pub blue: Srgb<u8>,
-    pub purple: Srgb<u8>,
-    pub cyan: Srgb<u8>,
-    pub white: Srgb<u8>,
-    pub bright_black: Srgb<u8>,
-    pub bright_red: Srgb<u8>,
-    pub bright_green: Srgb<u8>,
-    pub bright_yellow: Srgb<u8>,
-    pub bright_blue: Srgb<u8>,
-    pub bright_purple: Srgb<u8>,
-    pub bright_cyan: Srgb<u8>,
-    pub bright_white: Srgb<u8>,
+    pub fg: Srgb,
+    pub bg: Srgb,
+    pub black: Srgb,
+    pub red: Srgb,
+    pub green: Srgb,
+    pub yellow: Srgb,
+    pub blue: Srgb,
+    pub purple: Srgb,
+    pub cyan: Srgb,
+    pub white: Srgb,
+    pub bright_black: Srgb,
+    pub bright_red: Srgb,
+    pub bright_green: Srgb,
+    pub bright_yellow: Srgb,
+    pub bright_blue: Srgb,
+    pub bright_purple: Srgb,
+    pub bright_cyan: Srgb,
+    pub bright_white: Srgb,
+}
+
+macro_rules! srgb {
+    ($hex:expr) => {{
+        let c = color_hex::color_from_hex!($hex);
+        // Convert from u8 [0, 255] to Srgb<f32> [0.0, 1.0]
+        Srgb::new(
+            c[0] as f32 / 255.0,
+            c[1] as f32 / 255.0,
+            c[2] as f32 / 255.0,
+        )
+    }};
 }
 
 pub const DEFAULT_COLORS: &ColorProfile = &ColorProfile {
-    // #cccccc
-    fg: Srgb::new(0xcc, 0xcc, 0xcc),
-    // #1f1f1f
-    bg: Srgb::new(0x1f, 0x1f, 0x1f),
-    // #080808
-    black: Srgb::new(0x08, 0x08, 0x08),
-    // #d20000
-    red: Srgb::new(0xd2, 0x00, 0x00),
-    // #6a9955
-    green: Srgb::new(0x6a, 0x99, 0x55),
-    // #f0e68c
-    yellow: Srgb::new(0xf0, 0xe6, 0x8c),
-    // #0d73cc
-    blue: Srgb::new(0x0d, 0x73, 0xcc),
-    // #772fb0
-    purple: Srgb::new(0x77, 0x2f, 0xb0),
-    //  #279370
-    cyan: Srgb::new(0x27, 0x93, 0x70),
-    // #cccccc
-    white: Srgb::new(0xcc, 0xcc, 0xcc),
-    // #1f1f1f
-    bright_black: Srgb::new(0x1f, 0x1f, 0x1f),
-    // #ff2727
-    bright_red: Srgb::new(0xff, 0x27, 0x27),
-    // #84bf6a
-    bright_green: Srgb::new(0x84, 0xbf, 0x6a),
-    // #ffff55
-    bright_yellow: Srgb::new(0xff, 0xff, 0x55),
-    // #1a8fff
-    bright_blue: Srgb::new(0x1a, 0x8f, 0xff),
-    // #695abc
-    bright_purple: Srgb::new(0x69, 0x5a, 0xbc),
-    // #31b98d
-    bright_cyan: Srgb::new(0x31, 0xb9, 0x8d),
-    // #9d9d9d
-    bright_white: Srgb::new(0x9d, 0x9d, 0x9d),
+    // #fcfcfc
+    fg: srgb!("#fcfcfc"),
+    // #232627
+    bg: srgb!("#232627"),
+    // ##232627
+    black: srgb!("#232627"),
+    // #ed1515
+    red: srgb!("#ed1515"),
+    // #11d116
+    green: srgb!("#11d116"),
+    // #f67400
+    yellow: srgb!("#f67400"),
+    // #1d99f3
+    blue: srgb!("#1d99f3"),
+    // #9b59b6
+    purple: srgb!("#9b59b6"),
+    // #1abc9c
+    cyan: srgb!("#1abc9c"),
+    // #fcfcfc
+    white: srgb!("#fcfcfc"),
+    // #7f8c8d
+    bright_black: srgb!("#7f8c8d"),
+    // #c0392b
+    bright_red: srgb!("#c0392b"),
+    // #1cdc9a
+    bright_green: srgb!("#1cdc9a"),
+    // #fdbc4b
+    bright_yellow: srgb!("#fdbc4b"),
+    // #3daee9
+    bright_blue: srgb!("#3daee9"),
+    // #8e44ad
+    bright_purple: srgb!("#8e44ad"),
+    // #16a085
+    bright_cyan: srgb!("#16a085"),
+    // #ffffff
+    bright_white: srgb!("#ffffff"),
 };
